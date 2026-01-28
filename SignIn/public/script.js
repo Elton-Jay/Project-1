1844fd8f459f29dd88f8431c1a5eb05327383f09
Elton Jay <barnidoelton@gmail.com>
2026-01-28 14:02:54 +0800

    second commit

---
+++ SignIn/public/script.js
@@ -0,0 +1,6 @@
+async function submitLogin(){
+    const user = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    
+    const response = await fetch()
+}
