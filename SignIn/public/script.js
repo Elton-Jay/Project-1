52e11fbc70a9356b9470a3cc232d5c768075ddb1
Elton Jay <barnidoelton@gmail.com>
2026-01-30 14:36:19 +0800

    just added some code

--- SignIn/public/script.js
+++ SignIn/public/script.js
@@ -2,5 +2,23 @@ async function submitLogin(){
     const user = document.getElementById('email').value;
     const pass = document.getElementById('password').value;
     
-    const response = await fetch()
+    try{
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            header: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({username: user, password: pass})
+        });
+        const result = await response.json;
+        document.getElementById('message').innerText = result.message;
+
+        if(result.success){
+            document.getElementById('message').style.color('green');
+        }
+        else{
+            document.getElementById('message').style.color('red');
+        }
+    }
+    catch(error){
+        document.getElementById('message').innerText = "Serve is not running!";
+    }
 }
--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static('LogInPage'))
+
+app.post('api/login', (req, res) => {
+    const {username, password} = req.body;
+
+    const users = JSON.parse(fs.ReadFileSync('user.json'))
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if(user)
+})
